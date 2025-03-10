2b36482b43732bc13f5a1d5d8f33f6d50faea5a6
khushichawla <khushichawlaemails@gmail.com>
2025-03-11 00:45:55 +0800

    User auth backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
 
 
 // app config
@@ -19,6 +21,7 @@ connectDB();
 // api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static("uploads")) // /images endpoint that exposes the uploads folder
+app.use("/api/user", userRouter)
 
 
 // http method to request data from server, other methods like delete, post etc
